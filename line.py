ba6779352ed0bddfafb97755bc676636d94c5558
JbCoder <JbCoder@users.noreply.github.com>
2020-05-01 16:32:30 +0100

    added frame spacing
    still need to calculate speed

--- line.py
+++ line.py
@@ -1,13 +1,16 @@
 class Line:
-        def __init__(self,colour,identity,x1,y1,x2,y2,angle,direction):
-                self.type = colour
+        def __init__(self,ltype,identity,x1,y1,x2,y2,flipped,leftExtended,rightExtended,multiplier=0):
+                self.type = ltype
                 self.id = identity
                 self.x1 = x1
                 self.y1 = y1
                 self.x2 = x2
                 self.y2 = y2
-                self.angle = 0
-                self.direction = False
+                self.flipped = flipped
+                self.leftExtended = leftExtended
+                self.rightExtended = rightExtended
+                if self.id == 1:
+                        self.multiplier = multiplier
 
         def getLine(self,identifier):
                 self.id = identifier
--- mp4toAnimate.py
+++ mp4toAnimate.py
@@ -1,6 +1,44 @@
 import random, math, os, sys,json
 from track import *
 from line import *
-import skvideo.io
-videodata = skvideo.io.vread("a.mov")  
-print(videodata.shape)
+import cv2
+MAXRGBFORBLACK = 40
+frames = []
+vidcap = cv2.VideoCapture('test.mov')
+success,image = vidcap.read()
+count = 0
+while success:
+	#cv2.imwrite("%d.jpg" % count, image)     # save frame as JPEG file      
+	frames.append(image)
+	success,image = vidcap.read()
+	print('Read a new frame: ', success)
+	count += 1
+# frames[frame][x][y]
+# pixel
+# 1,2,3 = red,green,blue
+#print(frames[0][20][30])
+track = Track()
+c = 0
+PIXELWIDTH = 1
+OFFSET = 0
+width = len(frames[0])
+height = len(frames[0][0])
+print(width, height)
+for n,frame in enumerate(frames):
+	for x,row in enumerate(frame):
+		for y,pixel in enumerate(row):
+			if pixel[0] < MAXRGBFORBLACK and pixel[1] < MAXRGBFORBLACK and pixel[2] < MAXRGBFORBLACK:
+				#print(x,y)
+				x1 = x + OFFSET - PIXELWIDTH/2
+				y1 = y + PIXELWIDTH/2
+				x2 = x + OFFSET - PIXELWIDTH/2
+				y2 = y - PIXELWIDTH/2
+				track.addLine(Line(2,c,x1,y1,x2,y2,False,False,False))
+				c += 1
+	OFFSET += width
+	print(f"Completed Frame {n}")
+track.addLine(Line(0,c, 0, height, OFFSET, height, False,False,False))
+
+
+track.data["startPosition"] = {"x":0,"y":height-5}
+track.saveTrack("test")
